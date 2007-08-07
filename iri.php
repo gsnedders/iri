3448537bfd6f7791dbfee61f08514cda9c99c8c9
Geoffrey Sneddon <geoffers@gmail.com>
2007-08-07 12:32:53 +0100

    Allow IRI::replace_invalid_with_pct_encoding() to normalise more, and improve comments.

--- iri.php
+++ iri.php
@@ -48,6 +48,21 @@ class IRI
 	const hexdigit = '0123456789ABCDEFabcdef';
 	
 	/**
+	 * Don't change case
+	 */
+	const same_case = 1;
+	
+	/**
+	 * Change to lowercase
+	 */
+	const lowercase = 2;
+	
+	/**
+	 * Change to uppercase
+	 */
+	const uppercase = 4;
+	
+	/**
 	 * Whether the object represents a valid IRI
 	 *
 	 * @var bool
@@ -103,27 +118,69 @@ class IRI
 	 *
 	 * @param string $string Input string
 	 * @param string $valid_chars Valid characters
+	 * @param int $case Normalise case
 	 * @return string
 	 */
-	private function replace_invalid_with_pct_encoding($string, $valid_chars)
+	private function replace_invalid_with_pct_encoding($string, $valid_chars, $case = self::same_case)
 	{
+		// Normalise case
+		if ($case & self::lowercase)
+		{
+			$string = strtolower($string);
+		}
+		elseif ($case & self::upppercase)
+		{
+			$string = strtoupper($string);
+		}
+		
+		// Store position and string length (to avoid constantly recalculating this)
 		$position = 0;
 		$strlen = strlen($string);
+		
+		// Loop as long as we have invalid characters, advancing the position to the next invalid character
 		while (($position += strspn($string, $valid_chars, $position)) < $strlen)
 		{
+			// If we have a % character
 			if ($string[$position] === '%')
 			{
+				// If we have a pct-encoded section
 				if ($position + 2 < $strlen && strspn($string, self::hexdigit, $position + 1, 2))
 				{
-					$string = substr_replace(substr($string, $position + 1, 2), strtoupper(substr($string, $position + 1, 2)), $position + 1, 2);
+					// Get the the represented character
+					$chr = chr(hexdec(substr($string, $position + 1, 2)));
+					
+					// If the character is valid, replace the pct-encoded with the actual character while normalising case
+					if (strpos($valid_chars, $chr) !== false)
+					{
+						if ($case & self::lowercase)
+						{
+							$chr = strtolower($chr);
+						}
+						elseif ($case & self::upppercase)
+						{
+							$chr = strtoupper($chr);
+						}
+						$string = substr_replace($string, $chr, $position + 1, 2);
+						$strlen -= 2;
+						$position++;
+					}
+					
+					// Otherwise just normalise the pct-encoded to uppercase
+					else
+					{
+						$string = substr_replace($string, strtoupper(substr($string, $position + 1, 2)), $position + 1, 2);
+						$position += 3;
+					}
 				}
+				// If we don't have a pct-encoded section, just replace the % with its own esccaped form
 				else
 				{
 					$string = substr_replace($string, '%25', $position, 1);
 					$strlen += 2;
+					$position += 3;
 				}
-				$position += 3;
 			}
+			// If we have an invalid character, change into its pct-encoded form
 			else
 			{
 				$string = str_replace($string[$position], strtoupper(dechex(ord($string[$position]))), $string, $count);
@@ -234,7 +291,7 @@ class IRI
 		}
 		else
 		{
-			$this->host = $this->replace_invalid_with_pct_encoding($host, self::reg_name);
+			$this->host = $this->replace_invalid_with_pct_encoding($host, self::reg_name, self::lowercase);
 			return true;
 		}
 	}
@@ -261,3 +318,4 @@ class IRI
 		$this->path = $this->replace_invalid_with_pct_encoding($path, self::path);
 		return true;
 	}
+}
